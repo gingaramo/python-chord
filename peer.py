d66279a5c6697ab921ac1e33a4ade7939e2a5736
Gastón Ingaramo <gaston770@gmail.com>
2014-01-05 12:04:13 -0300

    naive Chord dht, rigid and correct implementation

--- peer.py
+++ peer.py
@@ -3,30 +3,55 @@ import json
 import socket
 import threading
 
+
+# log size of the ring
+LOGSIZE = 8
+# size of the ring
+SIZE = 1<<LOGSIZE
+
+# helper function to determine if a key falls within a range
 def inrange(c, a, b):
-	# c in [a,b)
+	"""
+		is c in [a,b)?, if a == b then it assumes a full circle
+		on the DHT, so it returns True.
+	"""
 	if a < b:
 		return a <= c and c < b
 	return a <= c or c < b
-
-SIZE = 8
 
 class Address(object):
 	def __init__(self, ip, port):
 		self.ip = ip
 		self.port = int(port)
 
-	def hash(self):
-		return hash(("%s:%s" % (self.ip, self.port))) % (1<<SIZE)
+	def __hash__(self):
+		return hash(("%s:%s" % (self.ip, self.port))) % SIZE
+
+	def __cmp__(self, other):
+		return other.__hash__() < self.__hash__()
+
+	def __eq__(self, other):
+		return other.__hash__() == self.__hash__()
 
 	def __str__(self):
 		return "<%s>:%s" % (self.ip, self.port)
 
+# decorator
+def requires_connection(func):
+	""" initiates and cleans up connections with remote server """
+	def inner(self, *args, **kwargs):
+		self.open_connection()
+		ret = func(self, *args, **kwargs)
+		self.close_connection()
+		return ret
+	return inner
+
+# class representing a remote peer
 class Remote(object):
 	def __init__(self, remote_address):
 		self.address_ = remote_address
 
-	def start_connection(self):
+	def open_connection(self):
 		self.socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket_.connect((self.address_.ip, self.address_.port))
 
@@ -37,70 +62,79 @@ class Remote(object):
 	def __str__(self):
 		return "Remote %s" % self.address_
 
-	def __unicode__(self):
-		return "Remote %s" % self.address_
-
 	def id(self):
-		return self.address_.hash()
+		return self.address_.__hash__()
 
+	def send(self, msg):
+		self.socket_.sendall(msg)
+#		print "send: %s <%s>" % (msg, self.address_)
+
+	def recv(self):
+		result = ""
+		while 1:
+			data = self.socket_.recv(256)
+			if len(data) == 0:
+				break
+			result += data
+		return json.loads(result)
+
+	@requires_connection
+	def get(self, key):
+		self.send('get %s' % key)
+		response = self.recv()
+
+		return response['response']
+
+	@requires_connection
+	def set(self, key, value):
+		self.send('set %s %s' % (key, value))
+		
+	@requires_connection
 	def successor(self):
-		self.start_connection()
-		self.socket_.sendall('get_successor')
+		self.send('get_successor')
 
-		response = json.loads(self.socket_.recv(1024))
-		print "send: %s <%s> r = '%s'" % ('get_successor', self.address_, response)
-		self.close_connection()
+		response = self.recv()
 		return Remote(Address(response['ip'], response['port']))
 
+	@requires_connection
 	def predecessor(self):
-		self.start_connection()
-		self.socket_.sendall('get_predecessor')
+		self.send('get_predecessor')
 
-		response = json.loads(self.socket_.recv(1024))
-		print "send: %s <%s> r = '%s'" % ('get_predecessor', self.address_, response)
-		self.close_connection()
+		response = self.recv()
 		return Remote(Address(response['ip'], response['port']))
 
-	def set_predecessor(self, address):
-		self.start_connection()
-		print "send: %s <%s>" % ('set_predecessor %s %s' % (address.ip, address.port), self.address_)
-		self.socket_.sendall('set_predecessor %s %s' % (address.ip, address.port))
-		self.close_connection()
-
+	@requires_connection
 	def find_successor(self, id):
-		self.start_connection()
-		self.socket_.sendall('find_successor %s' % id)
-		data = self.socket_.recv(1024)
+		self.send('find_successor %s' % id)
 
-		response = json.loads(data)
-		print "send: %s <%s> r = '%s'" % ('find_successor %s' % id, self.address_, response)
-		self.close_connection()
+		response = self.recv()
 		return Remote(Address(response['ip'], response['port']))
 
+	@requires_connection
+	def set_predecessor(self, address):
+		self.send('set_predecessor %s %s' % (address.ip, address.port))
+
+	@requires_connection
 	def closest_preceding_finger(self, id):
-		self.start_connection()
-		self.socket_.sendall('closest_preceding_finger %s' % id)
+		self.send('closest_preceding_finger %s' % id)
 
-		response = json.loads(self.socket_.recv(1024))
-		print "send: %s <%s> r = '%s'" % ('closest_preceding_finger %s' % id, self.address_, response)
-		self.close_connection()
+		response = self.recv()
 		return Remote(Address(response['ip'], response['port']))
 
+	@requires_connection
 	def update_finger_table(self, address, i):
-		self.start_connection()
-		self.socket_.sendall('update_finger_table %s %s %s' % (address.ip, address.port, i))
-		print "send: %s <%s>" % ('update_finger_table %s %s %s' % (address.ip, address.port, i), self.address_)
-		self.close_connection()
+		self.send('update_finger_table %s %s %s' % (address.ip, address.port, i))
 
-
-class Peer(threading.Thread):
-	def __init__(self, local_address, remote_address = None):
+# deamon to run local's run method
+class Daemon(threading.Thread):
+	def __init__(self, local):
 		threading.Thread.__init__(self)
-		self.local_ = Local(local_address, remote_address)
+		self.local_ = local
 
 	def run(self):
 		self.local_.run()
 
+# class representing a local peer
 class Local(object):
 	def __init__(self, local_address, remote_address = None):
 		self.address_ = local_address
@@ -110,22 +144,29 @@ class Local(object):
 		self.socket_.bind((self.address_.ip, int(self.address_.port)))
 		self.socket_.listen(100)
 
+		self.data_ = {}
+
 		if remote_address:
 			remote = Remote(remote_address)
-			self.finger_ = range(0, SIZE)
+			self.finger_ = range(0, LOGSIZE)
 			self.init_finger_table(remote)
 			self.update_others()
 		else:
+			# first node on ring
 			self.finger_ = list()
-			for i in range(0, SIZE):
+			for i in range(0, LOGSIZE):
 				self.finger_.append(self)
 			self.predecessor_ = self
+
+		# start the daemon
+		self.daemon_ = Daemon(self)
+		self.daemon_.start()
 
 	def __del__(self):
 		self.socket_.close()
 
 	def id(self):
-		return self.address_.hash()
+		return self.address_.__hash__()
 
 	def successor(self):
 		return self.finger_[0]
@@ -136,58 +177,67 @@ class Local(object):
 
 	def find_predecessor(self, id):
 		node = self
-		# we are alone
+		# we are alone in the ring
 		if node.successor() == node:
 			return node
-		# we are not alone
-		while not inrange(id, (node.id() + 1) % (1<<SIZE), (node.successor().id() + 1) % (1<<SIZE)):
+		# we are not alone in the ring
+		while not inrange(id, (node.id() + 1) % SIZE, (node.successor().id() + 1) % SIZE):
 			node = node.closest_preceding_finger(id)
 		return node
 
 	def closest_preceding_finger(self, id):
-		for i in range(SIZE-1,-1,-1):
-			# finger[i].node_id in (n, id)?
+		for i in range(LOGSIZE-1,-1,-1):
 			if inrange(self.finger_[i].id(), self.id() + 1, id):
 				return self.finger_[i]
 		return self
 
-	# join
 	def init_finger_table(self, remote):
-		self.finger_[0] = remote.find_successor((self.id() + 1) % (1<<SIZE))
+		self.finger_[0] = remote.find_successor((self.id() + 1) % SIZE)
 		self.predecessor_ = self.finger_[0].predecessor()
 		self.finger_[0].set_predecessor(self.address_)
-		for i in range(1, SIZE):
-			if inrange(((self.id() + (1<<i)) % (1<<SIZE)), self.id(), self.finger_[i-1].id()):
+		for i in range(1, LOGSIZE):
+			if inrange(((self.id() + (1<<i)) % SIZE), self.id(), self.finger_[i-1].id()):
 				self.finger_[i] = self.finger_[i-1]
 			else:
-				self.finger_[i] = remote.find_successor((self.id() + (1<<i)) % (1<<SIZE))
+				self.finger_[i] = remote.find_successor((self.id() + (1<<i)) % SIZE)
 
 	def update_others(self):
-		for i in range(0, SIZE):
-			previous = self.find_predecessor((self.id() - (1<<(i)) + 1 + (1<<SIZE)) % (1<<SIZE))
+		for i in range(0, LOGSIZE):
+			previous = self.find_predecessor((self.id() - (1<<(i)) + 1 + SIZE) % SIZE)
 			# not nessesary, but we avoid sending a message
 			if previous != self:
 				previous.update_finger_table(self.address_, i)
 
 	def update_finger_table(self, remote, i):
-		if inrange(remote.id(), (self.id() + (1<<i)) % (1<<SIZE), self.finger_[i].id() + 1):
+		if inrange(remote.id(), (self.id() + (1<<i)) % SIZE, self.finger_[i].id() + 1):
 			self.finger_[i] = remote
 			# do not update if previous is predecesor (optimization)
-			if self.predecessor_.address_.hash() != remote.address_.hash():
+			if self.predecessor_.address_.__hash__() != remote.address_.__hash__():
 				self.predecessor_.update_finger_table(remote.address_, i)
+
+	def get(self, key):
+		try:
+			return self.data_[key]
+		except Exception:
+			return None
+
+	def set(self, key, value):
+		self.data_[key] = value
 
 	def run(self):
 		while 1:
 			conn, addr = self.socket_.accept()
 			request = conn.recv(1024)
+			# telnet connection
+			if request[-2:] == "\r\n":
+				request = request[:-2]
+
 			print "recv: '%s' <%s>" % (request, addr)
 			request = request.split(' ')
 			if request[0] == 'get_successor':
 				conn.sendall(json.dumps({'ip': self.successor().address_.ip,
 										 'port': self.successor().address_.port}))
 			if request[0] == 'get_predecessor':
-				print json.dumps({'ip': self.predecessor_.address_.ip,
-										 'port': self.predecessor_.address_.port})
 				conn.sendall(json.dumps({'ip': self.predecessor_.address_.ip,
 										 'port': self.predecessor_.address_.port}))
 
@@ -205,12 +255,12 @@ class Local(object):
 			if request[0] == 'set_predecessor':
 				self.predecessor_ = Remote(Address(request[1], int(request[2])))
 			if request[0] == 'get':
-				pass
+				conn.sendall(json.dumps({'response': self.get(request[1])}))
 			if request[0] == 'set':
-				pass
+				self.set(request[1], request[2])
 			if request[0] == 'print_finger_table':
 				res = {}
-				for i in range(0, SIZE):
+				for i in range(0, LOGSIZE):
 					res[i] = "%s" % self.finger_[i].address_
 				conn.sendall(json.dumps(res))
 
@@ -220,11 +270,23 @@ class Local(object):
 				self.socket_.close()
 				break
 
+# data structure that represents a distributed hash table
+class DHT(threading.Thread):
+	def __init__(self, local_address, remote_address = None):
+		self.local_ = Local(local_address, remote_address)
+
+	def get(self, key):
+		remote = self.local_.find_successor(hash(key))
+		return remote.get(key)
+
+	def set(self, key, value):
+		remote = self.local_.find_successor(hash(key))
+		remote.set(key, value)
+
 if __name__ == "__main__":
 	import sys
 	if len(sys.argv) == 2:
 		local = Local(Address("127.0.0.1", sys.argv[1]))
 	else:
 		local = Local(Address("127.0.0.1", sys.argv[1]), Address("127.0.0.1", sys.argv[2]))
-	print "self id = %s " % local.id()
 	local.run()
--- test.py
+++ test.py
@@ -1,41 +1,51 @@
 import os
 import socket
+import random
 from peer import *
+
 
 def check_finger_table_integrity(id, finger_table, hash_list):
 	print "checking the finger table integrity of %s" % id
-	for i in range(0, SIZE):
+	for i in range(0, LOGSIZE):
 		finger_id = finger_table[i].id()
 		# finger_id is in [id + 2^i, id)
-		assert inrange(finger_id, (id + (1<<i)) % (1<<SIZE), id)
+		assert inrange(finger_id, (id + (1<<i)) % SIZE, id)
 		for j in range(0, len(hash_list)):
 			if (hash_list[j] == finger_id):
 				continue
 			# there's no other finger in between
-			assert (id + (1<<i)) % (1<<SIZE) == finger_id or not inrange(hash_list[j], (id + (1<<i)) % (1<<SIZE), finger_id)
+			assert (id + (1<<i)) % SIZE == finger_id or not inrange(hash_list[j], (id + (1<<i)) % SIZE, finger_id)
 
-address_list = map(lambda addr: Address('127.0.0.1', addr), range(10100, 10200, 7))
-hash_list 	 = map(lambda addr: addr.hash(), address_list)
-peers_list   = []
+# create addresses
+address_list = map(lambda addr: Address('127.0.0.1', addr), range(10100, 10400, 7))
+# keep unique ones
+address_list = sorted(set(address_list))
+# hash the addresses
+hash_list 	 = map(lambda addr: addr.__hash__(), address_list)
+# create the nodes
+locals_list   = []
 for i in range(0, len(address_list)):
-	if i == 0:
-		peer = Peer(address_list[i])
-	else:
-		peer = Peer(address_list[i], address_list[i-1])
-	peers_list.append(peer)
-	peer.start()
-print hash_list
-print "done creating peers, test pid %s" % os.getpid()
+	try:
+		if len(locals_list) == 0:
+			local = Local(address_list[i])
+		else:
+			# use a random already created peer's address
+			# as a remote
+			local = Local(address_list[i], locals_list[random.randrange(len(locals_list))].address_)
+		locals_list.append(local)
+	except Exception:
+		pass
+
+print "done creating peers, our pid %s is" % os.getpid()
 hash_list.sort()
-print hash_list
 
+# check integrity
+for local in locals_list:
+	check_finger_table_integrity(local.id(), local.finger_, hash_list)
 
-for peer in peers_list:
-	check_finger_table_integrity(peer.local_.id(), peer.local_.finger_, hash_list)
-
-for peer in peers_list:
+# shutdown peers
+for local in locals_list:
 	msocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	msocket.connect((peer.local_.address_.ip, peer.local_.address_.port))
+	msocket.connect((local.address_.ip, local.address_.port))
 	msocket.sendall('shutdown')
 	msocket.close()
-
